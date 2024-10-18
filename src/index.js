044b92772036249de0f58bb7fd3464f202e8a940
Kevin Carter <console.logkevin@gmail.com>
2024-10-18 14:41:04 -0400

    init application, update readme, update file structure

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import react and react dom
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// get reference to root
+const element = document.getElementById("root");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// tell react to take control of that element
+const root = ReactDOM.createRoot(element);
+
+// show the component on the screen 
+root.render(<App />);
