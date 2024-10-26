eb0096f764a72c845b72ab56de118110576d6a2d
Kevin Carter <console.logkevin@gmail.com>
2024-10-26 09:52:54 -0400

    save prgress

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -6,11 +6,49 @@ const Hero = () => {
     <div className="hero--container">
 
       <div className="hero--card-container">
+        
         <div className="hero--card">
           <img className="hero--card-img" src={landImg} alt="landscape aerial view" />
-
+          
+          <div className="hero--gradient"></div>
           <div className="hero--card-text-box">
             <h6 className="hero--card-category">space</h6>
+            <h1>SpaceX releases $800M worth of aerial footage</h1>
+            
+            <div className="hero--card-details">
+              <p>Kevin Carter</p>
+              <div>- 13 hours ago</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="hero--card">
+          <img className="hero--card-img" src={landImg} alt="landscape aerial view" />
+          
+          <div className="hero--gradient"></div>
+          <div className="hero--card-text-box">
+            <h6 className="hero--card-category">space</h6>
+            <h1>SpaceX releases $800M worth of aerial footage</h1>
+            
+            <div className="hero--card-details">
+              <p>Kevin Carter</p>
+              <div>- 13 hours ago</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="hero--card">
+          <img className="hero--card-img" src={landImg} alt="landscape aerial view" />
+          
+          <div className="hero--gradient"></div>
+          <div className="hero--card-text-box">
+            <h6 className="hero--card-category">space</h6>
+            <h1>SpaceX releases $800M worth of aerial footage</h1>
+            
+            <div className="hero--card-details">
+              <p>Kevin Carter</p>
+              <div>- 13 hours ago</div>
+            </div>
           </div>
         </div>
 
