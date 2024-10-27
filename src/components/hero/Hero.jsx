3c2587e64a75b08f5b3021fa4f425cfe2913ab1f
Kevin Carter <console.logkevin@gmail.com>
2024-10-27 12:20:31 -0400

    work on hero section of the page

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -5,8 +5,7 @@ const Hero = () => {
   return (
     <div className="hero--container">
 
-      <div className="hero--card-container">
-        
+      <div className="hero--card-container">        
         <div className="hero--card">
           <img className="hero--card-img" src={landImg} alt="landscape aerial view" />
           
@@ -16,8 +15,9 @@ const Hero = () => {
             <h1>SpaceX releases $800M worth of aerial footage</h1>
             
             <div className="hero--card-details">
-              <p>Kevin Carter</p>
-              <div>- 13 hours ago</div>
+              <div>
+                <span className="hero--writer">Kevin Carter</span> - <span className="hero--time">13 hours ago</span>
+              </div>
             </div>
           </div>
         </div>
@@ -31,8 +31,9 @@ const Hero = () => {
             <h1>SpaceX releases $800M worth of aerial footage</h1>
             
             <div className="hero--card-details">
-              <p>Kevin Carter</p>
-              <div>- 13 hours ago</div>
+              <div>
+                <span className="hero--writer">Kevin Carter</span> - <span className="hero--time">13 hours ago</span>
+              </div>
             </div>
           </div>
         </div>
@@ -46,16 +47,81 @@ const Hero = () => {
             <h1>SpaceX releases $800M worth of aerial footage</h1>
             
             <div className="hero--card-details">
-              <p>Kevin Carter</p>
-              <div>- 13 hours ago</div>
+              <div>
+                <span className="hero--writer">Kevin Carter</span> - <span className="hero--time">13 hours ago</span>
+              </div>
             </div>
           </div>
         </div>
-
-        
-        
       </div>
 
+      <div className="hero--headline-container">
+        <h3 className="hero--headline-title">Top Headlines</h3>
+
+        <div className="hero--list">
+
+          <div className="hero--item">
+            <div className="hero--square-case">
+              <div className="hero--item-square"></div>
+            </div>
+            <p className="hero--item-description">The Hisense C2 
+              Ultra is a well-rounded, great-looking home 
+              projector
+            </p>
+          </div>
+
+          <div className="hero--item">
+            <div className="hero--square-case">
+              <div className="hero--item-square"></div>
+            </div>
+            <p className="hero--item-description">
+              FTC hits Lyft with $2.1M penalty for luring drivers 
+              with false earnings claims
+            </p>
+          </div>
+
+          <div className="hero--item">
+            <div className="hero--square-case">
+              <div className="hero--item-square"></div>
+            </div>
+            <p className="hero--item-description">
+              ‘Yikes’: While gaming, Musk inadvertently broadcasts 
+              ‘scary’ near-abort of Starship booster landing
+            </p>
+          </div>
+
+          <div className="hero--item">
+            <div className="hero--square-case">
+              <div className="hero--item-square"></div>
+            </div>
+            <p className="hero--item-description">
+              User’s guide to TechCrunch Disrupt 2024
+            </p>
+          </div>
+
+          <div className="hero--item">
+            <div className="hero--square-case">
+              <div className="hero--item-square"></div>
+            </div>
+            <p className="hero--item-description">
+              The fediverse is getting its own TikTok competitor 
+              called Loops
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="hero--shapes">
+        <div className="shapes--row-1">
+          <div className="shapes--dark"></div>
+          <div className="shapes--light"></div>
+        </div>
+
+        <div className="row-2">
+          <div className="shapes--none"></div>
+          <div className="shapes--middle"></div>
+        </div>
+      </div>
     </div>
   );
 };
