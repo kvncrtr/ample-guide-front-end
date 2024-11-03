42d0ae4f08eac772743760ae6dfa1eda638a9f7c
Kevin Carter <console.logkevin@gmail.com>
2024-11-03 08:11:34 -0500

    update store files

---
+++ src/store/src/reducer.js
@@ -0,0 +1,89 @@
+let id = 0;
+
+function reducer(state = [], action) {
+   if (action.type === "ADD_POST") {
+      return [
+         ...state, 
+         {
+            id: ++id,
+            issn: "",
+            about: "",
+            abstract: "",
+            accessMode: "",
+            accessModeSufficient: "",
+            accountablePerson: "",
+            aggregateRating: "",
+            alternativeHeadline: "",
+            audience: "",
+            author: action.payload.author,
+            citation: "",
+            comment: "",
+            commentCount: 0,
+            conditionsOfAccess: "",
+            contentRating: "",
+            contributor: "",
+            copyrightHolder: "",
+            copyrightNotice: "",
+            copyrightYear: 2024,
+            countryOfOrigin: "",
+            creativeWorkStatus: "",
+            creditText: "",
+            dateCreated: "",
+            dateModified: "",
+            datePublished: "",
+            discussionUrl: "",
+            editor: "",
+            educationLevel: "",
+            educationalUse: "",
+            encodingFormat: "",
+            expires: "",
+            funder: "",
+            genre: "",
+            hasPart: false,
+            headline: "",
+            inLanguage: "",
+            interactionStatistic: "",
+            isAccessibleForFree: true,
+            isFamilyFriendly: true,
+            isPartOf: "",
+            keywords: "",
+            license: "",
+            locationCreated: "",
+            mentions: "",
+            offers: "",
+            position: 0,
+            publisher: "",
+            schemaVersion: "1.0.0",
+            size: 0.0,
+            sponsor: "",
+            teaches: "",
+            text: action.payload.text,
+            thumbnail: {},
+            thumbnailUrl: "",
+            timeRequired: "",
+            typicalAgeRange: "",
+            version: "1.0.0",
+            video: {},
+            videoUrl: "",
+            additionalType: "",
+            description: "",
+            name: "",
+            potentialAction: "",
+            url: "",
+         },
+      ]
+   } else if (action.type === "REMOVE_POST") {
+      return state.filter(post => post.id !== action.payload.id)
+   } else if (action.type === "FREE_POST") {
+      const newArr = [...state];
+      newArr.find(post => {
+         if (post.id === action.payload.id) {
+            console.log(post);
+         }
+      })
+   }
+
+   return state;
+}
+
+export default reducer;
